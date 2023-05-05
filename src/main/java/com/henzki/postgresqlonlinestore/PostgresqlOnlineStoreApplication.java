42d12c7933dc69d8ca40c0f31372715b9cc07f50
henzki <hennajau@hotmail.com>
2023-05-05 11:08:10 +0300

    Pohja postgresql projektille

---
+++ src/main/java/com/henzki/postgresqlonlinestore/PostgresqlOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.henzki.postgresqlonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresqlOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henzki/postgresqlonlinestore/PostgresqlOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henzki.postgresqlonlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresqlOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
